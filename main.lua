34a8e48dbfa379a1f799f57241e905820b75cf4f
Ecludian <charelnugroho@gmail.com>
2022-03-27 13:43:00 +0700

    The Ball Update

--- main.lua
+++ main.lua
@@ -18,6 +18,7 @@ function love.load()
     -- nearest-neightbor filtering on upscaling and downscaling to prevent text blurry
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    math.randomseed(os.time())
     -- create font object
     smallFont = love.graphics.newFont('font.ttf', 8)
 
@@ -40,6 +41,12 @@ function love.load()
         vsync = true
     })
 
+    ballX = V_WIDTH/2 -2
+    ballY = V_HEIGHT/2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
     --Initialize score variable
     player1Score = 0
     player2Score = 0
@@ -47,22 +54,30 @@ function love.load()
     player1Y = 30
     player2Y = V_HEIGHT - 50
   
+
+    gamestate = 'start'
 end
 
 function love.update(dt)
     --player 1 paddle control
     if love.keyboard.isDown('w')then
-        player1Y = player1Y + -paddleSpeed * dt
+        player1Y = math.max(0, player1Y + -paddleSpeed * dt)
     elseif love.keyboard.isDown('s')then
-        player1Y = player1Y + paddleSpeed * dt
+        player1Y = math.min(V_HEIGHT-20, player1Y + paddleSpeed * dt)
     end
         
     --player 2 paddle control
     if love.keyboard.isDown('up')then
-        player2Y = player2Y + -paddleSpeed * dt
+        player2Y = math.max(0, player2Y + -paddleSpeed * dt)
     elseif love.keyboard.isDown('down')then
-        player2Y = player2Y + paddleSpeed * dt
+        player2Y = math.min(V_HEIGHT-20, player2Y + paddleSpeed * dt)
     end
+
+    if gamestate == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
+
 end
 
 -- Keyboard Handling
@@ -70,6 +85,20 @@ function love.keypressed(key)
     --keys can be accessed by string name
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gamestate == 'start' then
+            gamestate = 'play'
+        else
+            gamestate = 'start'
+            
+            --start the ball in the middle of the screen
+            ballX = V_WIDTH/2-2
+            ballY = V_HEIGHT/2-2
+            
+            --give ball x and y velocity a random starting number
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
     end
 end
 
@@ -81,6 +110,7 @@ function love.draw()
    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
     love.graphics.setFont(smallFont)
+    if gamestate == 'start' then
     love.graphics.printf(
         'Hello Pong!', -- text to ender
         0,             --  starting x (center)
@@ -89,6 +119,9 @@ function love.draw()
         V_WIDTH,       -- number of pixels to center within
         'center'       -- alignment mode, center, left, or right
     )
+    else
+    love.graphics.printf('PLAY', 0, 20, V_WIDTH, 'center')
+    end
 
     -- set score to use scroefont
     love.graphics.setFont(scoreFont)
@@ -107,7 +140,7 @@ function love.draw()
     -- Right paddles
     love.graphics.rectangle('fill', V_WIDTH - 15, player2Y, 5, 20)
     -- BALL
-    love.graphics.rectangle('fill', V_WIDTH / 2 - 2, V_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
  
     push:apply('end')
     
